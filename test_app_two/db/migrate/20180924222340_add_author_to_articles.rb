79db230f1824e187257c0bb66d6156ba834bc668
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-10-05 13:03:53 -0700

    changes

--- test_app_two/db/migrate/20180924222340_add_author_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthorToArticles < ActiveRecord::Migration
-  def change
-    add_column :articles, :author, :string
-    add_column :articles, :created_at, :datetime
-    add_column :articles, :updated_at, :datetime
-  end
-end
