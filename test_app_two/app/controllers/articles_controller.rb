f7fc4bfb013e60a995a191c89292377a6522a0f8
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-10-05 12:23:31 -0700

    now the app can delete things.

--- test_app_two/app/controllers/articles_controller.rb
+++ test_app_two/app/controllers/articles_controller.rb
@@ -8,6 +8,13 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Articles was successfully deleted."
+    redirect_to articles_path
+  end
+
   def show
     @article = Article.find(params[:id])
   end
