6a51623ee483c968f8bb6ed0d58ac0615a0d0827
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-10-05 12:36:19 -0700

    now we are using before_action to call methods before they are created.

--- test_app_two/app/controllers/articles_controller.rb
+++ test_app_two/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  # This will ruby that it needs to call the method set_article before it calls the other methods.
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def new
     @article = Article.new
@@ -9,23 +11,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Articles was successfully deleted."
     redirect_to articles_path
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(articles_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to article_path(@article)
@@ -49,6 +47,12 @@ class ArticlesController < ApplicationController
     # redirect_to articles_path(@article)
   end
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+
     def articles_params
       params.require(:article).permit(:title, :description)
     end
